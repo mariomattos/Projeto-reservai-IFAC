3bfd36cbda734eb2f4122c89d494953a941554d4
Caio Marcos <caiosarabelo@gmail.com>
2026-06-22 20:08:05 -0500

    feat(database): cria User no banco de dados.
    O banco de dados foi criado. Com isso, a tabela User pôde ser adicionada.

---
+++ reservaí IFAC/database/DB - Reservaí.session.sql
@@ -0,0 +1,11 @@
+/* CREATE DATABASE Reservaí;
+USE Reservaí;
+
+CREATE TABLE User (
+    registrationId INT PRIMARY KEY,
+    userCpf VARCHAR(11) NOT NULL,
+    userBirthDate DATE NOT NULL,
+    userEmail VARCHAR(100) NOT NULL,
+    userPassword VARCHAR(100) NOT NULL,
+    userName VARCHAR(100) NOT NULL
+); */
